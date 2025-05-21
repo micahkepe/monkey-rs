ffff932a22544654f1920a153446668321de0376
micahkepe <micahkepe@gmail.com>
2025-05-21 10:18:34 -0700

    Enhance REPL greeting message

--- src/repl.rs
+++ src/repl.rs
@@ -6,10 +6,23 @@ use std::io::{stdin, stdout, Write};
 use crate::lexer;
 use crate::token;
 
+/// The prompt to display to the user
 const PROMPT: &str = ">> ";
 
 /// Runs a simple REPL for the user to run Monkey code.
 pub fn start() {
+    println!(
+        r"
+       __  ___          __
+      /  |/  /__  ___  / /_____ __ __
+     / /|_/ / _ \/ _ \/  '_/ -_) // /
+    /_/  /_/\___/_//_/_/\_\\__/\_, /
+                              /___/
+        "
+    );
+    println!("Welcome to the Monkey programming language!");
+    println!("Feel free to type in commands\n");
+
     loop {
         print!("{}", PROMPT);
         let _ = stdout().flush();
